7f88468d3ca7d55f279daf16574c2aa34b422b4d
citrafardiani-24 <citrafardiani@gmail.com>
2025-09-21 11:07:31 +0700

    first commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    
+    public function kelasC($nama = "", $npm = "", $kelas = "")
+{
+    // Cek apakah ada foto sesuai nama
+    $fotoPath = public_path("images/{$nama}.jpg");
+    $foto = file_exists($fotoPath) ? "images/{$nama}.jpg" : "images/foto2.jpeg";
+
+    $data = [
+        'nama' => $nama,
+        'npm' => $npm,
+        'kelas' => $kelas,
+        'foto' => $foto
+    ];
+
+    return view('kelasC', $data);
+}
+
+    
+}
+
+
